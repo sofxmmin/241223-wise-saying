d937f4b76c29377f29e35e4e9247670b18e7c4e4
siwoo <namsm17@daum.net>
2024-12-23 15:07:55 +0900

    work2

---
+++ src/main/java/com/WiseSaying/Main.java
@@ -0,0 +1,18 @@
+package com.WiseSaying;
+
+public class Main {
+    public static void main(String[] args) {
+
+        App app = new App();
+        app.run();
+
+    }
+}
+
+class App {
+
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+        System.out.println("명령) 종료:");
+    }
+}

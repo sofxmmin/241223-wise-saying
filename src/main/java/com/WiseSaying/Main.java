a63669a00a6dd310c9a0c13290d57a952825b9ab
siwoo <namsm17@daum.net>
2024-12-23 15:20:38 +0900

    Work3

--- src/main/java/com/WiseSaying/Main.java
+++ src/main/java/com/WiseSaying/Main.java
@@ -1,5 +1,7 @@
 package com.WiseSaying;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -12,7 +14,18 @@ public class Main {
 class App {
 
     public void run() {
+
+        Scanner scanner = new Scanner(System.in);
+
+
         System.out.println("== 명언 앱 ==");
+        System.out.print("명령 : ");
+        scanner.nextLine(); //입력값 가져옴. 입력값이 없으면 기다린다.
+
+        System.out.print("작가 : ");
+        scanner.nextLine();
+
+        System.out.println("1번 명언이 등록되었습니다.");
         System.out.println("명령) 종료:");
     }
 }
